c18ceedd493ad771c49d1f9e44ab631e06970ad6
Jhon Jairo Roa <jhonjairoroa87@gmail.com>
2014-11-20 21:48:18 -0500

    addede status message and added latitude format validation

--- web/js/controllers.js
+++ web/js/controllers.js
@@ -3,7 +3,7 @@ var socrexControllers = angular.module('socrex.controllers', ['ui.bootstrap']);
 socrexControllers.controller('newListingFormCtrl', ['$scope' , '$rootScope' , '$http' , '$location', function ($scope, $rootScope, $http, $location) {
 
     $scope.listingFormData = {};
-   
+    $scope.statusMessage = "";
 
 
     $scope.onSubmitNewListing = function(){
@@ -11,9 +11,25 @@ socrexControllers.controller('newListingFormCtrl', ['$scope' , '$rootScope' , '$
         this.saveListing(listingDataToSend);
     }
 
+    $scope.parseIntValue = function(keyArray, formData){
+        for (var i = 0 ; i < keyArray.length ; i++){
+            var keyElement = keyArray[i];
+            if(keyElement in formData){
+                formData[keyElement] = parseInt(formData[keyElement]);
+            }
+        }
+    }
+
     $scope.processListingData  = function(formData){
-        // set pinctures array
-        formData.pictures = $scope.generatePicturesArray(formData.picturesString);
+        // set pictures array string
+        formData.pictures = JSON.stringify($scope.generatePicturesArray(formData.picturesString));
+        formData.move_in = formData.move_in.split("-").join("");
+        formData.source = formData.source + "-manual";
+
+        var keyArray = ['amenities' , 'cieling' , 'classic', 'deck_balcony' , 'hardwood' , 'kitchen' , 'laundry' , 'lighting'
+                        , 'loft', 'modern','parking','pet' , 'spacing', 'studio', 'sublet_roomate', 'view']
+
+        $scope.parseIntValue(keyArray, formData);
         // delete picturesString attribute from formData object
         delete formData.picturesString;
         // return formadata
@@ -27,15 +43,18 @@ socrexControllers.controller('newListingFormCtrl', ['$scope' , '$rootScope' , '$
 
     $scope.saveListing = function(requestObj){
         // do call to server to save preferences
+        $scope.statusMessage = "Saving listing into database ... ";
         var responsePromise = $http({
             //url: 'http://127.0.0.1:5000/listings/filter', 
-            url: 'http://byopapp-api-stage.herokuapp.com/listings',
+            url: 'http://byopapp-api-dev.herokuapp.com/listings',
             method: 'POST',
             data: $.param(requestObj),
             headers: {'Content-Type': 'application/x-www-form-urlencoded'}
         });
 
         responsePromise.success(function(data, status, headers, config) {
+            $scope.statusMessage = "The listing was succesfully saved";
+            $scope.listingFormData = {};
             console.log("Succeeded response");
             //$rootScope.currentListingFilter = data.Data.PreferenceId.$oid;
             //var preferenceId = data.Data.PreferenceId.$oid;
@@ -43,6 +62,7 @@ socrexControllers.controller('newListingFormCtrl', ['$scope' , '$rootScope' , '$
         });
         
         responsePromise.error(function(data, status, headers, config) {
+            $scope.statusMessage = "There was an error saving the listing, please try again";
             console.log("Succeeded response - error");
         }); 
     }
